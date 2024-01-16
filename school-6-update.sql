d1ba07262d887ec7eaa58b5aba9bcfabe3cf720d
Bergen Carloss <bergen.carloss@kellogg.northwestern.edu>
2024-01-16 15:07:31 +0000

    school update

---
+++ school-6-update.sql
@@ -0,0 +1,3 @@
+UPDATE students
+SET phone = "293-499-3445"
+WHERE id = 1;
---
+++ school-8-insert-again.sql
@@ -0,0 +1,11 @@
+INSERT INTO students (
+    first_name,
+    last_name,
+    phone)
+
+VALUES (
+    "Ben"
+    "Block"
+    "234-394-9999")
+)
+)
